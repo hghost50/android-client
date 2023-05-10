e30a0e0302cbcc463ce06f59017faa0a2c31817f
hghost50 <119769197+hghost50@users.noreply.github.com>
2023-05-10 15:56:30 +0000

    Update BaseUrl.java

--- mifosng-android/src/main/java/com/mifos/api/BaseUrl.java
+++ mifosng-android/src/main/java/com/mifos/api/BaseUrl.java
@@ -11,9 +11,9 @@ package com.mifos.api;
 public class BaseUrl {
 
     public static final String PROTOCOL_HTTPS = "https://";
-    public static final String API_ENDPOINT = "bank.ebedou.com";
+    public static final String API_ENDPOINT = "192.168.8.100";
     public static final String API_PATH = "/fineract-provider/api/v1/";
-    public static final String PORT = "80";
+    public static final String PORT = "8443";
     // "/" in the last of the base url always
 
     public String getName() {
